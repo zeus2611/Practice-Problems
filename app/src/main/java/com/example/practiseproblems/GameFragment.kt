0167bd5e6ee814c556d57cce755f015bcb3c43cb
Nischay <nischay473@gmail.com>
2021-04-19 12:06:44 +0530

    Design Update

--- app/src/main/java/com/example/practiseproblems/GameFragment.kt
+++ app/src/main/java/com/example/practiseproblems/GameFragment.kt
@@ -18,11 +18,29 @@ class GameFragment : Fragment() {
 
     private var questions: MutableList<Question> = mutableListOf(
         Question(
-            text = "What is the color of android Mascot?",
-            answers = listOf()
+            text = "Predict the output of below C++ programs.\n" +
+                    "// Assume that integers take 4 bytes.\n" +
+                    "#include<iostream>\n" +
+                    "  \n" +
+                    "using namespace std;   \n" +
+                    "  \n" +
+                    "class Test\n" +
+                    "{\n" +
+                    "  static int i;\n" +
+                    "  int j;\n" +
+                    "};\n" +
+                    "  \n" +
+                    "int Test::i;\n" +
+                    "  \n" +
+                    "int main()\n" +
+                    "{\n" +
+                    "    cout << sizeof(Test);\n" +
+                    "    return 0;\n" +
+                    "}",
+            answers = listOf("A", "B", "C", "D")
         ),
         Question(
-            text = "",
+            text = "What is the color of android Mascot?",
             answers = listOf()
         ),
         Question(
@@ -78,6 +96,15 @@ class GameFragment : Fragment() {
         )
 
         binding.questionText.setText(questions[0].text)
+
+        answer = currentQuestion.answers.toMutableList()
+
+        binding.optionA.setText(answer[0])
+        binding.optionB.setText(answer[1])
+        binding.optionC.setText(answer[2])
+        binding.optionD.setText(answer[3])
+        binding.optionD.setBackgroundResource(R.drawable.option_selected)
+
 
         val msg = "Question " + (questionIndex+1)
         binding.questionNo.setText(msg)
